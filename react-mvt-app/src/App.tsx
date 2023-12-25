ac43196928ecbf6aee451e83ddec62911eb41696
liheyuan <li@coder4.com>
2023-12-25 13:17:56 +0800

    MOD: login page

--- react-mvt-app/src/App.tsx
+++ react-mvt-app/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { NewsList } from "./pages/news-list";
+import { LoginPage } from "./pages/login";
 
 function App() {
   return (
     <div className="App">
-      <NewsList />
+      <LoginPage />
+      {/* <NewsList /> */}
     </div>
   );
 }
